b41302cece483989c24a0738879703c9465e7e8a
Steven Childrey <steven.childrey36@yahoo.com>
2017-02-22 10:34:36 -0500

    Initial controller configure

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -1,7 +1,9 @@
 'use strict'
 
 class AuthController {
-
+    * index(request, response) {
+            yield response.sendView('login')
+    }
 }
 
 module.exports = AuthController
--- app/Http/Controllers/RegisterController.js
+++ app/Http/Controllers/RegisterController.js
@@ -1,7 +1,9 @@
 'use strict'
 
 class RegisterController {
-
+    * index(request, response) {
+            yield response.sendView('newAccount')
+    }
 }
 
 module.exports = RegisterController
