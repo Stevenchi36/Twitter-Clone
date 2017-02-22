4e6df48054754d963f4e64a68615730755e183a8
Steven Childrey <steven.childrey36@yahoo.com>
2017-02-22 15:14:03 -0500

    Set up register

--- app/Http/Controllers/RegisterController.js
+++ app/Http/Controllers/RegisterController.js
@@ -1,8 +1,26 @@
 'use strict'
 
+const User = use('App/Model/User')
+const Hash = use('Hash')
+
 class RegisterController {
     * index(request, response) {
             yield response.sendView('newAccount')
+    }
+    
+    * doRegister(request, response) {
+        const user = new User()
+        user.username = request.input('name')
+        user.email = request.input('email')
+        user.password = yield Hash.make(request.input('password'))
+        
+        yield user.save()
+        
+        var registerMessage = {
+            success: 'Registration successful! Now login.'
+        }
+        
+        yield response.sendView('newAccount', { registerMessage : registerMessage })
     }
 }
 
