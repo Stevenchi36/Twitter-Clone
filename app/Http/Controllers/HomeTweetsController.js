f66536983d43432c67f788d414f992399054e3ce
Steven Childrey <steven.childrey36@yahoo.com>
2017-03-13 16:42:23 -0400

    working on tweets

--- app/Http/Controllers/HomeTweetsController.js
+++ app/Http/Controllers/HomeTweetsController.js
@@ -7,6 +7,14 @@ class HomeTweetsController {
         const tweets = yield Tweets.all()
         yield response.sendView('home', {tweets: tweets.toJSON() })
     }
+    
+    * store (request, response) {
+        const tweet = new Tweets()
+        tweet.content = request.input('content')
+        
+        const tweets = yield Tweets.all()
+        yield response.sendView('home', {tweets: tweets.toJSON() })
+    }
 }
 
 module.exports = HomeTweetsController
